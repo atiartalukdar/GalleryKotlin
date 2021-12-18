8fca7f1dab4b646b3d18fc6cf8c7eef6c7625e9d
Atiar Talukdar <swajan.talukdar@gmail.com>
2021-12-18 17:33:49 +0900

    Nagivation Component and Gallery fragment added.

---
+++ app/src/main/java/info/atiar/GalleryKotlin/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,8 @@
+package info.atiar.GalleryKotlin.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.atiar.GalleryKotlin.R
+
+class GalleryFragment: Fragment(R.layout.fragment_galary){
+
+}
