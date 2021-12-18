713c93e1b442cd5d760558ed9bc23547ae0232d1
Atiar Talukdar <swajan.talukdar@gmail.com>
2021-12-18 16:51:52 +0900

    view and photo model created

---
+++ app/src/main/java/info/atiar/GalleryKotlin/model/PhotoModel.kt
@@ -0,0 +1,32 @@
+package info.atiar.GalleryKotlin.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class PhotoModel(
+    val id: String,
+    val description: String?,
+    val urls: PhotoUrls,
+    val authors: Author
+):Parcelable {
+    @Parcelize
+    data class PhotoUrls(
+        val raw: String,
+        val full: String,
+        val regular: String,
+        val small: String,
+        val thumb: String
+    ):Parcelable {
+
+    }
+    @Parcelize
+    data class Author (
+        val name: String,
+        val username: String
+        ):Parcelable{
+        val attributionUrl get() = "https://unsplash.com/$username?utm_source=GalleryKotlin&utm_medium=referral"
+    }
+}
+
+
