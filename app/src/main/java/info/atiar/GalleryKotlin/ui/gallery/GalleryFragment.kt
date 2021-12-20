2237880e491e023d40d51d2a3b1e8384ef217cd6
Atiar Talukdar <swajan.talukdar@gmail.com>
2021-12-20 13:44:51 +0900

    RecyclerView adapter and PagingDataAdapter implemented

--- app/src/main/java/info/atiar/GalleryKotlin/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/info/atiar/GalleryKotlin/ui/gallery/GalleryFragment.kt
@@ -5,17 +5,34 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.atiar.GalleryKotlin.R
+import com.atiar.GalleryKotlin.databinding.FragmentGalaryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GalleryFragment: Fragment(R.layout.fragment_galary){
     private val viewModel by viewModels<GalleryViewModel>()
+    private var _binding: FragmentGalaryBinding? = null
+    private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.photos.observe(viewLifecycleOwner){
+        _binding = FragmentGalaryBinding.bind(view)
 
+        val adapter = PexelsPhotoAdapter()
+
+        binding.apply {
+            recyclerView.setHasFixedSize(true)
+            recyclerView.adapter = adapter
         }
+
+        viewModel.photos.observe(viewLifecycleOwner){
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
---
+++ app/src/main/java/info/atiar/GalleryKotlin/ui/gallery/PexelsPhotoAdapter.kt
@@ -0,0 +1,61 @@
+package info.atiar.GalleryKotlin.ui.gallery
+
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.atiar.GalleryKotlin.R
+import com.atiar.GalleryKotlin.databinding.ItemPhotoBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import info.atiar.GalleryKotlin.data.PhotoModel
+
+class PexelsPhotoAdapter : PagingDataAdapter<PhotoModel, PexelsPhotoAdapter.PhotoViewHolder>(
+    PHOTO_COMPARATOR
+) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        val binding = ItemPhotoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PhotoViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        val currentItem = getItem(position)
+
+        if (currentItem != null) {
+            holder.bind(currentItem)
+        }
+
+
+    }
+
+
+    class PhotoViewHolder(private val binding: ItemPhotoBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(photo: PhotoModel) {
+            binding.apply {
+                Glide.with(itemView)
+                    .load(photo.urls.regular)
+                    .centerCrop()
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .error(R.drawable.ic_error)
+
+                //textViewUserName.text = photo.authors.username
+            }
+        }
+    }
+
+
+    companion object {
+        private val PHOTO_COMPARATOR = object : DiffUtil.ItemCallback<PhotoModel>() {
+            override fun areItemsTheSame(oldItem: PhotoModel, newItem: PhotoModel) =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: PhotoModel, newItem: PhotoModel) =
+                oldItem == newItem
+        }
+    }
+}
